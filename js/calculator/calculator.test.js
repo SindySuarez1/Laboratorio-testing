682bc615deb639b41f899db1a6d3bd201256b921
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-08-04 17:06:34 -0500

    Arregle errores de la tarea 1

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -17,20 +17,20 @@ test('subtract -5 - -10 to equal 5', () => {
 });
 
 // ! TAREA 1
-const { divide, multiply} = require("./funciones.js")
+const { divide, multiply} = require('./calculator')
 
 test('divide 15 / 5 to equal 3', () => {
   expect(divide(15, 5)).toBe(3);
 });
 
-test('divide 191 / 5 to equal 3', () => {
+test('divide 191 / 5 to equal 38.2', () => {
   expect(divide(191, 5)).toBe(38.2);
 });
 
-test('multiply 5 * 4 to equal 1', () => {
-  expect(multiplyt(5, 4)).toBe(20);
+test('multiply 5 * 4 to equal 20', () => {
+  expect(multiply(5, 4)).toBe(20);
 });
 
-test('multiply 35 * 14 to equal 1', () => {
+test('multiply 35 * 14 to equal 490', () => {
   expect(multiply(35, 14)).toBe(490);
 });
