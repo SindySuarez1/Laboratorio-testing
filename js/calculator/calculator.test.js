e50f86ff3dca5b3434588ed32e895bec12da691f
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-08-04 16:57:21 -0500

    Agregue la solución de la tarea 1

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,22 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+// ! TAREA 1
+const { divide, multiply} = require("./funciones.js")
+
+test('divide 15 / 5 to equal 3', () => {
+  expect(divide(15, 5)).toBe(3);
+});
+
+test('divide 191 / 5 to equal 3', () => {
+  expect(divide(191, 5)).toBe(38.2);
+});
+
+test('multiply 5 * 4 to equal 1', () => {
+  expect(multiplyt(5, 4)).toBe(20);
+});
+
+test('multiply 35 * 14 to equal 1', () => {
+  expect(multiply(35, 14)).toBe(490);
+});
