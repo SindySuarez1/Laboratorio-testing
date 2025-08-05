c403e72020046bb640c84333f2de63e55cb5b240
caroo <dianacarolinacatama@gmail.com>
2025-08-05 14:58:41 -0500

    test final

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -19,18 +19,26 @@ test('subtract -5 - -10 to equal 5', () => {
 // ! TAREA 1
 const { divide, multiply} = require('./calculator')
 
-test('divide 15 / 5 to equal 3', () => {
+//prueba funcion divide
+test('divide 15 / 5 igual 3', () => {
   expect(divide(15, 5)).toBe(3);
 });
 
-test('divide 191 / 5 to equal 38.2', () => {
+test('divide 191 / 5 igual 38.2', () => {
   expect(divide(191, 5)).toBe(38.2);
 });
 
-test('multiply 5 * 4 to equal 20', () => {
+//prueba funcion multiply
+test('multiplicar 5 * 4 to igual 20', () => {
   expect(multiply(5, 4)).toBe(20);
 });
 
-test('multiply 35 * 14 to equal 490', () => {
+test('multiplicar 35 * 14 igual 490', () => {
   expect(multiply(35, 14)).toBe(490);
+});
+
+//! TAREA 3
+//prueba manejo caso division por 0
+test('verificar division 0', () => {
+  expect(divide(30, 0)).toBe("no se puede dividir por 0");
 });
--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -16,3 +16,54 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+//! TAREA 2
+//prueba para la función add() 
+test('add() agrega un usuario que no está en la lista', () => {
+    let user = new User(1221,"amigos", "amigos@generation.org");
+    userController.add(user);
+    expect(userController.getUsers()).toContain(user);
+});
+//prueba para la función remove() 
+test('remove() no afecta si el usuario no está en la lista', () => {
+    const user = new User(1221,"amigos", "amigos@generation.org");
+    userController.remove(user); // No se agrega
+    expect(userController.getUsers()).not.toContain(user);
+    expect(userController.getUsers().length).toBeGreaterThanOrEqual(0);
+});
+
+//pruebas funcion findByEmail()
+test('encontrar el usuario con email santiago@generation.org', () => {       
+  const expectedUser = {
+    id: 1234,
+    name: 'Santiago',
+    email: 'santiago@generation.org'
+  };
+  const result = userController.findByEmail('santiago@generation.org');
+  expect(result).toEqual(expectedUser);
+});
+
+test('encontrar el usuario con email santiago@generation.org', () => {       
+  const expectedUser = {
+    id: 1221,
+    name: 'amigos',
+    email: 'amigos@generation.org'
+  };
+const result = userController.findByEmail('amigos@generation.org');
+  expect(result).toEqual(expectedUser);
+});
+
+
+//pruebas funcion findById()
+test("find by Id should return the correct user", ()=> {
+    let user = new User(1234,"Santiago", "santiago@generation.org");
+    userController.add(user);
+    let find = userController.findById(1234);
+    expect(find).toStrictEqual(user);
+});
+
+test("find by Id should return the correct user", ()=> {
+    let user = new User(1221,"amigos", "amigos@generation.org");
+    userController.add(user);
+    let find = userController.findById(1221);
+    expect(find).toStrictEqual(user);
+});
